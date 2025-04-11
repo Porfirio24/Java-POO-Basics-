68d9b4b5bb6140df0ebbbbc947d3fb3e89d6dbec
Porfirio-Neto <netinholeite2607@gmail.com>
2025-04-11 11:46:19 -0300

    Atualização fundamentos

--- Src/Fundamentos/BoletimEstudantil.java
+++ Src/Fundamentos/BoletimEstudantil.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+
 public class BoletimEstudantil {
     public static void main(String[] args) {
-        int mediaFinal = 6;
+        Scanner scanner = new Scanner(System.in);
 
-        if (mediaFinal < 6)
-           System.out.println("Reprovado");
-        else if (mediaFinal == 6)
-           System.out.println("Na Media");
-        else
+        System.out.println ("Digite sua Primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Digite sua segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Digite sua Terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3)/3;
+
+        System.out.print("Media final: " + media);
+
+        if (media < 6.0) {
+            System.out.println(" Reprovado!");
+        } else if (media == 6.0) {
+            System.out.println("Na Média");
+        } else {
             System.out.println("Aprovado");
+        }
+
+        scanner.close();
     }
 }
