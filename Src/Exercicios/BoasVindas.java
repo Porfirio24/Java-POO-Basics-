69bdb61ff0b835d8b6eb00a7e37307612772c3d1
Porfirio-Neto <netinholeite2607@gmail.com>
2025-04-24 16:33:08 -0300

    Atualização Exercicios

---
+++ Src/Exercicios/BoasVindas.java
@@ -0,0 +1,15 @@
+package Src.Exercicios;
+import java.util.Scanner;
+
+public class BoasVindas {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Nome: ");
+
+        String nome = entrada.nextLine();
+        System.out.println("Bem-Vindo(a) " + nome);
+
+        entrada.close();
+    }
+}
