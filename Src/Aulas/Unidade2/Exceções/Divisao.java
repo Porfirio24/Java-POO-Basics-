5664d3c7440d883379eaf2f3f91ea5ce64dac72a
Porfirio-Neto <netinholeite2607@gmail.com>
2025-07-01 15:13:17 -0300

    Atualização Unidade2

--- Src/Aulas/Unidade2/Exceções/Divisao.java
+++ Src/Aulas/Unidade2/Exceções/Divisao.java
@@ -6,20 +6,49 @@ public class Divisao {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    int num, den;
+    int num = 0, den = 0;
+    boolean numeradorCorreto = false;
+    boolean denominadorCorreto = false;
 
-    System.out.print("Numerador: ");
-    num = sc.nextInt();
+    do {
+    try{
+      System.out.print("Numerador: ");
+      num = sc.nextInt();
+      numeradorCorreto = true;
+    } catch (Exception e){
+      System.out.println("Entrada inválida. O numerador deveria ser inteiro. ");
+    }
+    } while (!numeradorCorreto);
 
-    System.out.print("Denominador: ");
-    den = sc.nextInt();
+    do {
+    try{
+      System.out.print("Denominador: ");
+      den = sc.nextInt();
+      denominadorCorreto = true;
+    } catch (Exception e){
+      System.out.println("Entrada inválida. O denominador deveria ser inteiro. ");
+    }
+    } while (!denominadorCorreto);
 
     System.out.println("Resultado: " + num + "/" + den + " = " + divisaoInteira(num, den));
 
     sc.close();
   }
 
-  public static int divisaoInteira(int numerador, int denomidaor){
-    return numerador/denomidaor;
+  public static int divisaoInteira(int numerador, int denominador){
+    int resultado = 0;
+    try{
+      resultado = numerador/denominador;
+    } catch (ArithmeticException e){
+      System.out.println("Erro: Divisão por zero não é permitida");
+    }
+    return resultado;
+  }
+
+  public static int divisaoInteira2(int numerador, int denominador){
+    if(denominador == 0){
+    throw new ArithmeticException("Divisão por zero não é permitida. ");
+    }
+    return numerador / denominador;
   }
 }
